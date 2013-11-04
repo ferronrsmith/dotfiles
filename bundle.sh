b402171fabb6b1442f47fe390b56b6b47f4f0166
ferron <ferronrsmith@gmail.com>
2013-11-03 20:49:24 -0400

    added more packages

--- bundle.sh
+++ bundle.sh
@@ -13,5 +13,15 @@ git clone https://github.com/kien/ctrlp.vim.git
 git clone https://github.com/scrooloose/syntastic.git
 git clone https://github.com/tpope/vim-rake.git
 git clone https://github.com/Lokaltog/vim-powerline.git
-git clone https://github.com/thoughtbot/vim-rspec.git
+git clone https://github.com/msanders/snipmate.vim.git
+git clone https://github.com/tpope/vim-git.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/vim-scripts/taglist.vim.git
+git clone https://github.com/airblade/vim-rooter.git
+git clone https://github.com/tpope/vim-unimpaired.git
+git clone https://github.com/tpope/vim-ragtag.git
+git clone https://github.com/tpope/vim-endwise.git
+git clone https://github.com/vim-scripts/tComment.git
+
+
 #git clone https://github.com/tpope/vim-vividchalk.git
