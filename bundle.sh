e3471a05445871cf81bd57e582fc1e508010f6d6
ferron <ferronrsmith@gmail.com>
2013-12-08 14:10:59 -0400

    repo reorganization. move vim color schemes to vundle dependency

--- bundle.sh
+++ bundle.sh
@@ -1,1 +1,6 @@
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+cp -r vim/doc/ ~/.vim/doc
+cp -r vim/indent/ ~/.vim/indent
+cp -r vim/syntax/ ~/.vim/syntax
+cp -r vim/ftdetect/ ~/.vim/ftdetect
+cp -r vim/compiler/ ~/.vim/compiler
