0b5075c67d885b5af69e5a10ce8b87b4d28f3f61
ferron <ferronrsmith@gmail.com>
2013-12-15 18:26:22 -0400

    reorganize repo and added symlink for dotfiles

---
+++ linux_synlinks.sh
@@ -0,0 +1,4 @@
+ln -nfs ~/personal_git/dotfiles/.vimrc ~/.vimrc 
+ln -nfs ~/personal_git/dotfiles/.gitconfig ~/.gitconfig
+ln -nfs ~/personal_git/dotfiles/.zshrc_linux ~/.zshrc
+ln -nfs ~/personal_git/dotfiles/.tmux.conf ~/.tmux.conf
