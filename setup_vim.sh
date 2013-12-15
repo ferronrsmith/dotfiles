9f5c45799e74a78203db81bef2822123e66700c5
ferron <ferronrsmith@gmail.com>
2013-12-15 19:15:26 -0400

    added config

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,9 +1,30 @@
-echo ""
+#!/bin/bash
+echo "
+                    .-'''-.
+                   / .===. \\
+                   \/ 6 6 \/
+                   ( \___/ )
+      _________ooo__\_____/______________
+     /                                   \\
+    |   ferron   ferronrsmith@gmail.com   |
+     \_______________________ooo_________/
+                    |  |  |
+                    |_ | _|
+                    |  |  |
+                    |__|__|
+                    /-'Y'-\\
+                   (__/ \__)
+"
+
+echo " Starting configuration for vim\n"
 
 # clone vimfiles repo
 git clone https://github.com/ferronrsmith/vimfiles.git ~/personal_git/vimfiles
 cd ~/personal_git/vimfiles/
 git submodule init && git submodule update
+
+
+echo " Creating symlinks \n"
 
 # create a symlink to my personal vimrc and tmux.conf files
 ln -nfs ~/personal_git/dotfiles/.vimrc ~/.vimrc 
@@ -17,3 +38,6 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
 	ln -nfs ~/personal_git/dotfiles/.gitconfig_mac ~/.gitconfig
 	ln -nfs ~/personal_git/dotfiles/.zshrc_mac ~/.zshrc
 fi
+
+echo " Finish configuration for vim\n"
+
