7757f89f05f843a538a212afd16323c70a9bc7e9
ferron <ferronrsmith@gmail.com>
2013-12-15 18:30:04 -0400

    added mac git config to mac symlink file

--- mac_synlinks.sh
+++ mac_synlinks.sh
@@ -1,4 +1,4 @@
 ln -nfs ~/personal_git/dotfiles/.vimrc ~/.vimrc 
-ln -nfs ~/personal_git/dotfiles/.gitconfig ~/.gitconfig
+ln -nfs ~/personal_git/dotfiles/.gitconfig ~/.gitconfig_mac
 ln -nfs ~/personal_git/dotfiles/.zshrc_mac ~/.zshrc
 ln -nfs ~/personal_git/dotfiles/.tmux.conf ~/.tmux.conf
