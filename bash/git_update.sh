4b4d69a93566a66764c843f4d6cfbe2d28fc3656
ferron <ferronrsmith@gmail.com>
2016-03-05 03:33:41 -0400

    added update script

---
+++ bash/git_update.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: update.sh
+#
+#         USAGE: ./update.sh
+#
+#   DESCRIPTION:
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (),
+#  ORGANIZATION:
+#       CREATED: 01/29/2016 02:51
+#      REVISION:  ---
+#===============================================================================
+
+ignore_list=("adminMongo" "data_files" ".")
+
+
+function ignore {
+    for i in "${ignore_list[@]}"
+    do
+        if [ "${1}" == "${i}" ]; then
+            return 1
+        fi
+    done
+}
+
+find . -maxdepth 1 -type d | while read line
+do
+    folder="${line/.\//}"
+    if ignore "${folder}" ; then
+        (
+            cd "${folder}"
+            git pull --rebase &
+        )
+    else
+        echo "ignored folder : ${folder}"
+    fi
+done
