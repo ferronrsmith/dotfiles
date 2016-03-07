5155abd1224071fb725f44fd218e3351c9693c98
ferron <ferronrsmith@gmail.com>
2016-03-07 00:08:29 -0400

    updated cpu temperature endpoints new conditions for mac osx updated gitconfig for mac os x

--- hub/hub.sh
+++ hub/hub.sh
@@ -5,4 +5,4 @@ echo " Installing hub ..."
 # clone vimfiles repo
 git clone https://github.com/github/hub.git ~/personal_git/hub
 cd ~/personal_git/hub/
-rake install prefix=/usr/local
+./script/build -o ~/bin/hub
