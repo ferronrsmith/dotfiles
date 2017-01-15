ccfe57b1755691df71ebc98da81582cfe6141a94
ferron <ferronrsmith@gmail.com>
2017-01-15 14:38:52 -0400

    cleanup junk * remove all reference of G from my repos

--- hub/hub.sh
+++ hub/hub.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-echo " Installing hub ..."
+update=1
+if [ ! -d ~/personal_git/hub ]; then
+	echo " Installing hub ..."
+	git clone https://github.com/github/hub.git ~/personal_git/hub
+else
+	echo "Updating hub ..."
+	update=0
+fi
 
-# clone vimfiles repo
-git clone https://github.com/github/hub.git ~/personal_git/hub
+# switch dir
 cd ~/personal_git/hub/
-./script/build -o ~/bin/hub
+
+# check if update should be performed
+if [ "${update}" == 0 ]; then
+	git pull origin master
+fi
+
+# build step
+./script/build
+sudo cp bin/hub /usr/local/bin/hub
