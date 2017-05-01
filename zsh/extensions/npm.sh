624995934f9568b610573424c4163272123b258f
ferron <ferronrsmith@gmail.com>
2017-05-01 10:02:51 -0400

    added nvm upgrade script

--- zsh/extensions/npm.sh
+++ zsh/extensions/npm.sh
@@ -7,3 +7,11 @@ function npm_upgrade () {
       npm -g install "$package"
   done
 }
+
+function nvm_upgrade () {
+  (
+    cd "$NVM_DIR"
+    git fetch origin
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
+  ) && . "$NVM_DIR/nvm.sh"
+}
