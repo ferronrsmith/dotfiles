dc03d673dd29d9d3c1020c2fc3b573ae7c7bd9dd
ferron <ferronrsmith@gmail.com>
2023-05-09 16:14:29 -0400

    linux changes

--- zsh/extensions/layout.sh
+++ zsh/extensions/layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI2 --mode 2560x1080 --pos 1600x0 --rotate normal --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output eDP1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output DP1 --off --output HDMI1 --off --output HDMI2 --mode 2560x1440 --pos 1600x0 --rotate normal --output VIRTUAL1 --off
